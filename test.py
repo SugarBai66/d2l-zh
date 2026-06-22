ba1e4840fb1ce25c0a3bd53ee310a64bafd9ab04
hugo <3048415810@qq.com>
2026-06-22 08:30:40 +0800

    添加README.md

---
+++ test.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+class MLP(nn.Module):
+    # 声明带有模型参数的层，这⾥里里声明了了两个全连接层
+    def __init__(self, **kwargs):
+        # 调⽤用MLP⽗父类Block的构造函数来进⾏行行必要的初始化。这样在构造实例例时还可以指定其他函数
+        # 参数，如“模型参数的访问、初始化和共享”⼀一节将介绍的模型参数params
+        super(MLP, self).__init__(**kwargs)
+        self.hidden = nn.Linear(784, 256) # 隐藏层
+        self.act = nn.ReLU()
+        self.output = nn.Linear(256, 10) # 输出层
+    # 定义模型的前向计算，即如何根据输⼊入x计算返回所需要的模型输出
+    def forward(self, x):
+        a = self.act(self.hidden(x))
+        return self.output(a)
+X = torch.rand(2, 784)
+net = MLP()
+print(net)
+net(X)
+
